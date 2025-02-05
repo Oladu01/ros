a7ba73b8696262f4baaf83192b2451bf3f8b02a8
Oladu01 <147079495+Oladu01@users.noreply.github.com>
2025-02-05 20:44:25 +0500

    Update subscriber.cpp

--- src/turtlebot_controller/src/subscriber.cpp
+++ src/turtlebot_controller/src/subscriber.cpp
@@ -4,7 +4,7 @@
 
 ros::Publisher pub;
 
-// Callback function that gets executed when a new message is received
+
 void poseCallback(const turtlesim::Pose::ConstPtr& msg) {
     ROS_INFO("Turtle Position -> x: [%f], y: [%f], theta: [%f]", msg->x, msg->y, msg->theta);
     geometry_msgs::Twist my_vel;
@@ -18,15 +18,15 @@ int main(int argc, char **argv) {
     // Initialize the ROS node
     ros::init(argc, argv, "turtle_listener");
 
-    // Create a NodeHandle
+ 
     ros::NodeHandle nh;
 	
 	pub=nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 	
-    // Subscribe to the /turtle1/pose topic
+   
     ros::Subscriber sub = nh.subscribe("/turtle1/pose", 10, poseCallback);
 
-    // Keep the node running
+    
     ros::spin();
 
     return 0;
