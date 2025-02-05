f149b2b341184c9cd45b017a8b9c74e6231b634d
Oladu01 <147079495+Oladu01@users.noreply.github.com>
2025-02-05 20:43:55 +0500

    Update spawn_turtle.cpp

--- src/turtlebot_controller/src/spawn_turtle.cpp
+++ src/turtlebot_controller/src/spawn_turtle.cpp
@@ -2,21 +2,21 @@
 #include "turtlesim/Spawn.h"
 
 int main(int argc, char **argv) {
-    // Initialize the ROS node
+  
     ros::init(argc, argv, "spawn_turtle_client");
     ros::NodeHandle nh;
 
-    // Create a client that will call the /spawn service
+
     ros::ServiceClient client = nh.serviceClient<turtlesim::Spawn>("/spawn");
 
-    // Define the service request
+   
     turtlesim::Spawn srv;
     srv.request.x = 1.0;
     srv.request.y = 5.0;
     srv.request.theta = 0.0;
-    srv.request.name = "Turtle_Leonardo";  // Change "YOURNAME" to your preferred turtle name
-
-    // Call the service
+    srv.request.name = "Turtle_Leonardo"; 
+    
+  
     if (client.call(srv)) {
         ROS_INFO("Spawned a new turtle: %s", srv.response.name.c_str());
     } else {
